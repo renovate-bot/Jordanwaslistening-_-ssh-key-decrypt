8c4a68dd926341678dcff0e2494d70ef8f2482f4
Ian Hattendorf <ianh@axosoft.com>
2019-09-20 14:16:25 -0700

    Use eslint airbnb config

--- index.js
+++ index.js
@@ -1,156 +1,135 @@
-module.exports = main
+const util = require('util');
 
-var util = require('util');
-var debug;
-if (util.debuglog)
-  debug = util.debuglog('ssh-key-decrypt');
-else if (/\bssh-key-decrypt\b/i.test(process.env.NODE_DEBUG || ''))
-  debug = function()
-    {
-    var msg = util.format.apply(util, arguments);
+let debug;
+if (util.debuglog) debug = util.debuglog('ssh-key-decrypt');
+else if (/\bssh-key-decrypt\b/i.test(process.env.NODE_DEBUG || '')) {
+  debug = (...args) => {
+    // eslint-disable-next-line prefer-spread
+    const msg = util.format(...args);
+    // eslint-disable-next-line no-console
     console.error('%s %s', 'SSH-KEY-DECRYPT', msg);
-    };
-else
-  debug = function() {};
+  };
+} else debug = () => {};
 
-var crypto = require('crypto');
-var assert = require('assert');
+const crypto = require('crypto');
+const assert = require('assert');
 
-var keyBytes =
-  {
+const keyBytes = {
   'DES-EDE3-CBC': 24,
   'DES-CBC': 8,
   'AES-128-CBC': 16,
   'AES-192-CBC': 24,
-  'AES-256-CBC': 32
-  };
+  'AES-256-CBC': 32,
+};
 
-
-function main(data, passphrase, outEnc)
-  {
-  if (Buffer.isBuffer(data))
-    {
-    data = data.toString('ascii');
-    }
-
-  if (!outEnc)
-    {
-    outEnc = 'buffer';
-    }
-
-  // Make sure it looks like a RSA private key before moving forward
-  var lines = data.trim().split('\n');
-  assert.equal(lines[0], '-----BEGIN RSA PRIVATE KEY-----');
-  assert.equal(lines[lines.length - 1], '-----END RSA PRIVATE KEY-----');
-  var l = 1;
-
-  var result;
-  if (lines[1] === 'Proc-Type: 4,ENCRYPTED')
-    {
-    var dekInfo = lines[2];
-    assert.equal(dekInfo.slice(0, 10), 'DEK-Info: ');
-    dekInfo = dekInfo.slice(10).split(',');
-    var type = dekInfo[0];
-    var iv = new Buffer(dekInfo[1], 'hex');
-    assert.equal(lines[3], '');
-    var encData = lines.slice(4, -1).join('');
-    result = decrypt(encData, type, passphrase, iv, outEnc);
-    }
-  else
-    {
-    var data = lines.slice(1, -1).join('');
-    result = formatOut(data, outEnc);
-    }
-
-  return result;
-  }
-
-function formatOut(data, outEnc)
-  {
-  var result;
-  switch (outEnc)
-    {
+function formatOut(data, outEnc) {
+  let result;
+  switch (outEnc) {
     case 'base64':
       result = data;
       break;
 
     case 'buffer':
-      result = new Buffer(data, 'base64');
+      result = Buffer.from(data, 'base64');
       break;
 
     default:
-      result = new Buffer(data, 'base64').toString(outEnc);
+      result = Buffer.from(data, 'base64').toString(outEnc);
       break;
-    }
+  }
   return result;
-  }
-
-function decrypt(encData, type, passphrase, iv, outEnc)
-  {
-  debug('decrypt', type, outEnc);
-  var key = passphraseToKey(type, passphrase, iv);
-  var dec = crypto.createDecipheriv(type, key, iv);
-  var data = '';
-  data += dec.update(encData, 'base64', 'base64');
-  data += dec.final('base64');
-  return formatOut(data, outEnc);
-  }
+}
 
 // port of EVP_BytesToKey, as used when decrypting PEM keys
-function passphraseToKey(type, passphrase, salt)
-  {
+function passphraseToKey(type, passphrase, salt) {
   debug('passphraseToKey', type, passphrase, salt);
-  var nkey = keyBytes[type];
+  let nkey = keyBytes[type];
 
-  if (!nkey)
-    {
-    var allowed = Object.keys(keyBytes);
-    throw new TypeError('Unsupported type. Allowed: ' + allowed);
-    }
+  if (!nkey) {
+    const allowed = Object.keys(keyBytes);
+    throw new TypeError(`Unsupported type. Allowed: ${allowed}`);
+  }
 
-  var niv = salt.length;
-  var saltLen = 8;
-  if (salt.length !== saltLen)
-    salt = salt.slice(0, saltLen);
-  var mds = 16;
-  var addmd = false;
-  var md_buf;
-  var key = new Buffer(nkey);
-  var keyidx = 0;
+  let niv = salt.length;
+  const saltLen = 8;
+  if (salt.length !== saltLen) salt = salt.slice(0, saltLen);
+  const mds = 16;
+  let addmd = false;
+  let mdBuf;
+  const key = Buffer.alloc(nkey);
+  let keyidx = 0;
 
-  while (true)
-    {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
     debug('loop nkey=%d mds=%d', nkey, mds);
-    var c = crypto.createHash('md5');
+    const c = crypto.createHash('md5');
 
-    if (addmd)
-      c.update(md_buf);
-    else
-      addmd = true;
+    if (addmd) c.update(mdBuf);
+    else addmd = true;
 
-    if (!Buffer.isBuffer(passphrase))
-      c.update(passphrase, 'ascii');
-    else
-      c.update(passphrase);
+    if (!Buffer.isBuffer(passphrase)) c.update(passphrase, 'ascii');
+    else c.update(passphrase);
 
     c.update(salt);
-    md_buf = c.digest('buffer');
+    mdBuf = c.digest('buffer');
 
-    var i = 0;
-    while (nkey && i < mds)
-      {
-      key[keyidx++] = md_buf[i];
+    let i = 0;
+    while (nkey && i < mds) {
+      key[keyidx++] = mdBuf[i];
       nkey--;
       i++;
-      }
+    }
 
-    var steps = Math.min(niv, mds - i);
+    const steps = Math.min(niv, mds - i);
     niv -= steps;
     i += steps;
 
-    if ((nkey == 0) && (niv == 0)) break;
-    }
-
-  return key
+    if ((nkey === 0) && (niv === 0)) break;
   }
 
+  return key;
+}
+
+function decrypt(encData, type, passphrase, iv, outEnc) {
+  debug('decrypt', type, outEnc);
+  const key = passphraseToKey(type, passphrase, iv);
+  const dec = crypto.createDecipheriv(type, key, iv);
+  let data = '';
+  data += dec.update(encData, 'base64', 'base64');
+  data += dec.final('base64');
+  return formatOut(data, outEnc);
+}
+
+function main(data, passphrase, outEnc) {
+  if (Buffer.isBuffer(data)) {
+    data = data.toString('ascii');
+  }
+
+  if (!outEnc) {
+    outEnc = 'buffer';
+  }
+
+  // Make sure it looks like a RSA private key before moving forward
+  const lines = data.trim().split('\n');
+  assert.equal(lines[0], '-----BEGIN RSA PRIVATE KEY-----');
+  assert.equal(lines[lines.length - 1], '-----END RSA PRIVATE KEY-----');
+
+  let result;
+  if (lines[1] === 'Proc-Type: 4,ENCRYPTED') {
+    let dekInfo = lines[2];
+    assert.equal(dekInfo.slice(0, 10), 'DEK-Info: ');
+    dekInfo = dekInfo.slice(10).split(',');
+    const type = dekInfo[0];
+    const iv = Buffer.from(dekInfo[1], 'hex');
+    assert.equal(lines[3], '');
+    const encData = lines.slice(4, -1).join('');
+    result = decrypt(encData, type, passphrase, iv, outEnc);
+  } else {
+    const resultData = lines.slice(1, -1).join('');
+    result = formatOut(resultData, outEnc);
+  }
+
+  return result;
+}
+
+module.exports = main;
--- test.js
+++ test.js
@@ -1,90 +1,76 @@
-var crypto = require('crypto');
-var assert = require('assert');
-var fs = require('fs');
-var path = require('path');
-var t = require('tap');
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const tap = require('tap');
 
-var decrypt = require('./index.js');
+const decrypt = require('./index.js');
 
 // All the fixtures should decrypt to this key
-var unenc = path.resolve(__dirname, 'fixtures', 'id_rsa_unencrypted');
-unenc = new Buffer(fs.readFileSync(unenc, 'ascii')
-    .trim()
-    .split('\n')
-    .slice(1, -1)
-    .join(''), 'base64');
+let unenc = path.resolve(__dirname, 'fixtures', 'id_rsa_unencrypted');
+unenc = Buffer.from(fs.readFileSync(unenc, 'ascii')
+  .trim()
+  .split('\n')
+  .slice(1, -1)
+  .join(''), 'base64');
 
-var tests =
-  [
+function tryThis(fn, f, msg) {
+  tap.test(f, (t) => {
+    t.plan(1);
+    t.doesNotThrow(fn, msg);
+  });
+}
+
+function test(f) {
+  let file;
+  let fileData;
+
+  tryThis(() => {
+    file = path.resolve(__dirname, 'fixtures', `id_rsa_${f}`);
+    fileData = fs.readFileSync(file, 'ascii');
+  }, f, 'failed reading test key');
+
+  let data;
+  tryThis(() => {
+    assert(data = decrypt(fileData, 'asdf'));
+    assert(Buffer.isBuffer(data), 'should be buffer');
+  }, f, 'failed decryption');
+
+  let hex;
+  tryThis(() => {
+    assert(hex = decrypt(fileData, 'asdf', 'hex'));
+    assert.equal(typeof hex, 'string');
+    assert.equal(hex, data.toString('hex'));
+  }, f, 'failed hex decryption');
+
+  let base64;
+  tryThis(() => {
+    assert(base64 = decrypt(fileData, 'asdf', 'base64'));
+    assert.equal(typeof base64, 'string');
+    assert.equal(base64, data.toString('base64'));
+  }, f, 'failed base64 decryption');
+
+  tryThis(() => {
+    assert.equal(data.length, unenc.length);
+  }, f, 'length differs');
+
+  tryThis(() => {
+    for (let i = 0; i < data.length; i++) {
+      assert.equal(data[i], unenc[i], `differs at position ${i}`);
+    }
+  }, f, 'byte check');
+}
+
+
+let tests = [
   'aes128',
   'aes192',
   'aes256',
   'des3',
-  'des'
-  ];
+  'des',
+];
 
-tests = tests.map(function(t)
-  {
-  return 'enc_' + t + '_asdf';
-  });
+tests = tests.map((t) => `enc_${t}_asdf`);
 
 tests.push('unencrypted');
 
 tests.forEach(test);
-
-function test(f)
-  {
-  var file;
-  var fileData;
-
-  tryThis(function()
-    {
-    file = path.resolve(__dirname, 'fixtures', 'id_rsa_' + f)
-    fileData = fs.readFileSync(file, 'ascii');
-    }, f, 'failed reading test key');
-
-  var data;
-  tryThis(function()
-    {
-    assert(data = decrypt(fileData, 'asdf'));
-    assert(Buffer.isBuffer(data), 'should be buffer');
-    }, f, 'failed decryption');
-
-  var hex;
-  tryThis(function()
-    {
-    assert(hex = decrypt(fileData, 'asdf', 'hex'));
-    assert.equal(typeof hex, 'string');
-    assert.equal(hex, data.toString('hex'));
-    }, f, 'failed hex decryption');
-
-  var base64;
-  tryThis(function()
-    {
-    assert(base64 = decrypt(fileData, 'asdf', 'base64'));
-    assert.equal(typeof base64, 'string');
-    assert.equal(base64, data.toString('base64'));
-    }, f, 'failed base64 decryption');
-
-  tryThis(function()
-    {
-    assert.equal(data.length, unenc.length);
-    }, f, 'length differs');
-
-  tryThis(function()
-    {
-    for (var i = 0; i < data.length; i++)
-      {
-      assert.equal(data[i], unenc[i], 'differs at position ' + i);
-      }
-    }, f, 'byte check');
-  }
-
-function tryThis(fn, f, msg)
-  {
-  t.test(f, function (t)
-    {
-    t.plan(1)
-    t.doesNotThrow(fn, msg)
-    })
-  }
